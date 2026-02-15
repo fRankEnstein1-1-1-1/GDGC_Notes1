8e0120970e316382f67ca3e708e3c2cd9f571d63
fRankEnstein1-1-1-1 <frankensteinmenon@gmail.com>
2026-02-15 20:20:00 +0530

    host

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000" // backend URL
+    baseURL: "https://one-xuug.onrender.com" // backend URL
 });
 
 export default API;
